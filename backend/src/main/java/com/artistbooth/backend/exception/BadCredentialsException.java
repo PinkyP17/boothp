9d16076ec8c6c4344c762b8e6d08e6d36e69a8fe
PinkyP17 <lufeepink17@gmail.com>
2026-08-19 00:09:51 +0800

    chore:archived backend

--- backend/src/main/java/com/artistbooth/backend/exception/BadCredentialsException.java
+++
@@ -1,7 +0,0 @@
-package com.artistbooth.backend.exception;
-
-public class BadCredentialsException extends RuntimeException {
-    public BadCredentialsException(String message) {
-        super(message);
-    }
-}
